109c9a330b00db4329747e8811f3d6fd8292877b
dkw5877 <dkw5877@gmail.com>
2014-08-04 10:31:23 -0700

    initial commit

---
+++ ClearStyle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClearStyle
+//
+//  Created by user on 8/4/14.
+//  Copyright (c) 2014 someCompanyNameHere. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
