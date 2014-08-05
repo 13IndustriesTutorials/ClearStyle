8d77439691ded5f21810f05d718ba24f8699f1b8
dkw5877 <dkw5877@gmail.com>
2014-08-04 18:23:23 -0700

    add Pan gesture recognizer to cell

--- ClearStyle/TableViewCell.swift
+++ ClearStyle/TableViewCell.swift
@@ -12,16 +12,21 @@ import QuartzCore
 class TableViewCell: UITableViewCell {
 
     var gradientLayer:CAGradientLayer
+    var originalCenter:CGPoint
+    var deleteOnDragRelease:Bool
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
     
     init(style: UITableViewCellStyle, reuseIdentifier: String!)
     {
         
         self.gradientLayer = CAGradientLayer()
+        self.deleteOnDragRelease = false;
+        self.originalCenter = CGPointMake(0, 0)
+        
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         self.gradientLayer.frame =  self.bounds //set frame to size of cell
         self.gradientLayer.colors = [UIColor(white: 1.0, alpha: 0.2),
@@ -30,6 +35,11 @@ class TableViewCell: UITableViewCell {
         ]
         self.gradientLayer.locations = [0.0, 0.1, 0.95, 1.00]
         self.layer.insertSublayer(self.gradientLayer, atIndex: 0)
+        
+        //create gesture recognizer
+        let recognizer = UIPanGestureRecognizer(target: self, action: "handlePan:")
+        recognizer.delegate = self
+        self.addGestureRecognizer(recognizer)
     }
 
     override func setSelected(selected: Bool, animated: Bool) {
@@ -44,4 +54,75 @@ class TableViewCell: UITableViewCell {
         self.gradientLayer.frame =  self.bounds
     }
     
+    //override the gesture recognizer start
+    override func gestureRecognizerShouldBegin(gestureRecognizer: UIGestureRecognizer!) -> Bool
+    {
+        var translation = (gestureRecognizer as UIPanGestureRecognizer).translationInView(self.superview)
+
+        
+        if fabs(translation.x) > fabs(translation.y)
+        {
+            return true
+        }
+        
+        return false
+    }
+    
+    func handlePan(recognizer:UIPanGestureRecognizer)
+    {
+        if recognizer.state == UIGestureRecognizerState.Began
+        {
+            //set the current center when the pan begins
+            self.originalCenter = self.center
+        }
+        
+        if recognizer.state == UIGestureRecognizerState.Changed
+        {
+            //translate the center
+            var translation = recognizer.translationInView(self)
+            self.center = CGPointMake(self.originalCenter.x + translation.x, self.originalCenter.y + translation.y)
+            
+            //determine whether the item has been dragged far enough to delete
+            self.deleteOnDragRelease = self.frame.origin.x < -self.frame.size.width/2
+        }
+        
+        if recognizer.state == UIGestureRecognizerState.Ended
+        {
+            var originalFrame = CGRectMake(0, self.frame.origin.y, self.bounds.size.width, self.bounds.size.height)
+            if !self.deleteOnDragRelease {
+                UIView.animateWithDuration(0.2, animations: {
+                    //animation
+                    self.frame = originalFrame
+                    })
+            }
+        }
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
